e194fabb5643cea29054e55354373ca824387aae
Sebastian Zapata <pansezapata@gmail.com>
2025-07-28 14:27:04 -0500

    Se crea el .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CocktailsModule } from './cocktails/cocktails.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [CocktailsModule,
-  MongooseModule.forRoot('mongodb+srv://szapatar:PVQcogm7ymywbdwv@cluster0.rihybzv.mongodb.net/licores')
+  
+  ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRoot(process.env.DATABASEURL??""),
   ],
   controllers: [AppController],
   providers: [AppService],
